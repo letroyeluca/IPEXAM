6fbc4020a9ddafd2e45e3fc58e29db862fec0fa0
letroyeluca <luca.letroye@student.uantwerpen.be>
2025-01-12 10:53:47 +0100

    verbetering efficientie room.cpp

--- Room.cpp
+++ Room.cpp
@@ -20,51 +20,57 @@ void Room::render(sf::RenderWindow* window) const {
 void Room::update(sf::Event * event) {
     vector<Entity*> walls;
     vector<Entity*> Enemys;
+    vector<Entity*> Weapons;
     bool enemykilled = false;
     Position oldplayerpos {0,0};
     bool kamercheck = false;
+    bool update = false;
+
+    //loop door alle elementen in de kamer
     for (Entity* entity : entities) {
+
+        // als er een player tegen komt
         if (auto* player = dynamic_cast<Player*>(entity)){
+
+            // controlleer of deze loop als is gepasseerd
             if (!kamercheck) {
+
+                //slaag de oude positie van de player op
                 oldplayerpos = player->getPosition();
+
+                //update de player met de nieuwe positie
                 playerptr = player;
                 player->update(event);
                 kamercheck = true;
             }else{
                 entity->update(event);
             }
-
         }
+
+        //als er een muur word gevonden slaag die op in de lijst van muren
         if (auto* wall = dynamic_cast<Wall*>(entity)){
             walls.push_back(wall);
         }
 
+        // als er vijand word gevonden slaag die op in de lijst van vijanden
         if (auto* enemy = dynamic_cast<Enemy*>(entity)){
             Enemys.push_back(enemy);
         }
 
-
-    }
-    for (Entity* entity : entities) {
+        // als het een wapen vind zet die op de lijst met wapens
         if (auto* weapon = dynamic_cast<Weapon*>(entity)) {
-            if (weapon->standsOn(this->playerptr)) {
-                this->playerptr->setAttackPower(1);
-                this->removeEntity(weapon);
-                weapon->update(event);
-                entity->update(event);
-            }
-        }
-        if (auto* enemy = dynamic_cast<Enemy*>(entity)){
-            if(this->playerptr->standsOn(enemy) && this->playerptr->getAttackPower() == 1){
-                this->removeEntity(enemy);
-
-                enemykilled = true;
-                entity->update(event);
-
-            }
+            Weapons.push_back(weapon);
         }
     }
 
+
+    for(Entity* weapon: Weapons){
+        if (this->playerptr->standsOn(weapon)) {
+            this->playerptr->setAttackPower(1);
+            this->removeEntity(weapon);
+            weapon->update(event);
+        }
+    }
 
     for(Entity* entity : walls){
         if(entity->getPosition().x == playerptr->nieuwePos.x && entity->getPosition().y == playerptr->nieuwePos.y){
@@ -74,6 +80,14 @@ void Room::update(sf::Event * event) {
     }
 
     for(Entity* entity : Enemys){
+        // als de speler op de vijand gaat staan en het zwaard heeft opgepakt, verslaag de vijand
+        if(this->playerptr->standsOn(entity) && this->playerptr->getAttackPower() == 1){
+            this->removeEntity(entity);
+            enemykilled = true;
+            update = true;
+            entity->update(event);
+        }
+
         if(entity->getPosition().x == playerptr->nieuwePos.x && entity->getPosition().y == playerptr->nieuwePos.y && this->playerptr->getAttackPower() == 0){
             playerptr->setPosition(oldplayerpos);
 
